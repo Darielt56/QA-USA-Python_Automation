ba48d22799192eef770c58a01c50a29f374c5148
darielt56 <D.Taveras15@gmail.com>
2025-05-25 00:27:53 -0400

    First Commit

--- data.py
+++ data.py
@@ -1,6 +1,6 @@
 # File with constants
 
-URBAN_ROUTES_URL = ''
+URBAN_ROUTES_URL = 'https://cnt-847bce32-5935-45cd-a800-47c9059d430a.containerhub.tripleten-services.com/'
 ADDRESS_FROM ='East 2nd Street, 601'
 ADDRESS_TO = '1300 1st St'
 PHONE_NUMBER = '+1 123 123 12 12'
